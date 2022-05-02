2a675a7db624325a9289d77e4d0e69243c8c5336
Minh Triet <bindsu63@gmail.com>
2022-05-02 14:11:54 +0700

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,8 @@ import { GenreModule } from './genres/genre.module';
 @Module({
   imports: [
     TypegooseModule.forRoot(
-      'mongodb+srv://minhtriet:Subindhight1@estate.l3gxu.mongodb.net/flixtor?retryWrites=true&w=majority',
+      process.env.DB ||
+        'mongodb+srv://minhtriet:Subindhight1@estate.l3gxu.mongodb.net/flixtor?retryWrites=true&w=majority',
     ),
     MovieModule,
     CategoryModule,
